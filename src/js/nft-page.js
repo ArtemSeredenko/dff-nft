9a67dc8fc03ccdcff977c28a5097f4402592b8cc
Ilya <kulak1224@gmail.com>
2024-01-22 21:15:16 +0200

    fixing animations

--- src/js/nft-page.js
+++ src/js/nft-page.js
@@ -1,3 +1,23 @@
 const timer = document.querySelector('.timer');
-
 timer.classList.add('animated-element', 'animation-left');
+
+const animatedElements = document.querySelectorAll('.animated-element');
+
+let options = {
+  rootMargin: '0px',
+  threshold: 0.2,
+};
+
+let observer = new IntersectionObserver(addClassName, options);
+
+animatedElements.forEach(element => {
+  observer.observe(element);
+});
+
+function addClassName(entries) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    console.log(entry.target);
+    entry.target.classList.add('visible');
+  });
+}
