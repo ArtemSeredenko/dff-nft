184145ae2940c35c05bea8caab504f21bf3c3347
wowka130299 <wolodya700@icloud.com>
2024-01-22 12:12:28 +0100

    update

--- src/js/artist-page.js
+++ src/js/artist-page.js
@@ -23,3 +23,26 @@ document.addEventListener('DOMContentLoaded', function() {
     }, 2100);
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elements = document.querySelectorAll('.animated-element');
+
+    var isInViewport = function(element) {
+        var rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        );
+    };
+
+    var handleScroll = function() {
+        elements.forEach(function(element) {
+            if (isInViewport(element) && !element.classList.contains('visible')) {
+                element.classList.add('visible');
+            }
+        });
+    };
+
+    document.addEventListener('scroll', handleScroll);
+    handleScroll(); // Викликати функцію на початку для перевірки видимості елементів
+});
