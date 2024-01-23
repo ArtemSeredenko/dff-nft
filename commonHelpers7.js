7a16da99ef6069ddeb7de002255d09598a0ecc3f
ArtemSeredenko <mpdart2013@gmail.com>
2024-01-23 08:24:54 +0000

    Deploying to gh-pages from @ ArtemSeredenko/dff-nft@eb55c3232cac00a7774552569712a91709f9f885 🚀

--- commonHelpers7.js
+++ commonHelpers7.js
@@ -1,2 +1,2 @@
-import"./assets/nft-page-e21bc140.js";document.addEventListener("DOMContentLoaded",function(){var t=document.querySelectorAll(".filter-link");t.forEach(function(e){e.addEventListener("click",function(i){i.preventDefault(),n(),e.classList.add("filter-link-active")})});function n(){t.forEach(function(e){e.classList.remove("filter-link-active")})}});
+import"./assets/nft-page-766726c0.js";document.addEventListener("DOMContentLoaded",function(){var t=document.querySelectorAll(".filter-link");t.forEach(function(e){e.addEventListener("click",function(i){i.preventDefault(),n(),e.classList.add("filter-link-active")})});function n(){t.forEach(function(e){e.classList.remove("filter-link-active")})}});
 //# sourceMappingURL=commonHelpers7.js.map
